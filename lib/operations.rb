8b1954caea7f77405b47c388793490fa33ba3431
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-06-24 16:38:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,6 +10,9 @@ end
 
 def not_safe?(speed)
 	speed > 40 && speed < 60 ? false : true
+	# if speed is greater than 40 AND less than 60, #not_safe? returns false
+	# if speed is less than 40, #not_safe? returns true
+	# if speed is greater than 60, #not_safe? returns true
 end
 	
 
